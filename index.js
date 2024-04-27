6a4e04db96171627da2635fad3883bafd266c39b
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-27 19:25:00 +0600

    server post and get added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,49 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 // mongodb added here
+
+const uri = "mongodb+srv://zahidulislamhere:7eGxK7yanCZggzc0@cluster0.bq1drti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        
+        const travelCollection = client.db('travelDB').collection('travel')
+
+        app.get('/travels', async(req,res) =>{
+            const cursor = travelCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/travels', async(req,res) =>{
+            console.log(req.body);
+            const traveler = req.body
+            const result = await travelCollection.insertOne(traveler)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
